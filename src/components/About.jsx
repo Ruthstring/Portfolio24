006185d61a5a5dfd3844b94b2a8251e697bcb142
Ruth Cuellar <ruthcllp@gmail.com>
2025-04-07 17:08:17 +0200

    retouch layout on tablet

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,36 +11,38 @@ const About = () => {
   return (
     <div id="about" className="about-container pb-12">
       
-      <div className="about-grid grid grid-cols-5 md:grid-cols-12 grid-rows-auto md:grid-rows-auto h-screen 2xl:h-[80vh] ">
+      <div className="about-grid grid grid-cols-5 md:grid-cols-12 grid-rows-auto md:grid-rows-auto h-screen max-h-[800px]  ">
 
         {/* Title Section */}
         <div className="title row-start-1 items-start col-span-5 md:col-start-2 md:col-span-4 2xl:pt-28">
           <h1 className="about-title text-center md:text-right text-[50px] md:text-[100px] 2xl:text-[125px] leading-[50px] md:leading-[100px] mt-10">
             ABOUT <br /> ME
           </h1>
+          <p></p>
         </div>
 
         {/* Intro Text */}
-        <div className="intro-text row-start-2 col-start-2 row-span-2 col-span-7 md:col-span-6 md:col-start-6 2xl:col-span-5  md:row-start-1 md:pb-2 2xl:pb-6">
-          <p className="text-left text-[20px] md:text-[30px] 2xl:text-[50px] leading-8 md:leading-10">
+        <div className="intro-text row-start-2 col-start-2 row-span-2 col-span-7 md:col-span-6 md:col-start-6 2xl:col-span-5 md:row-start-1 md:pb-2 lg:pb-3 2xl:pb-0 ">
+          <p className="text-left text-[20px] md:text-[25px] lg:text-[35px] 2xl:text-[40px]  leading-8 2xl:leading-10">
             A hands-on, solution-oriented professional. Willing to keep growing.
           </p>
         </div>
         
 
         <div className="icons-container col-start-7 row-start-4 row-span-5 mt-16 mr-4 md:col-start-5 md:mt-10 md:items-left   ">
+        {/*Multi icon */}
         <div className="icons icon4 row-start-5 col-span-1 md:col-span-3 2xl:w-[100px] ">
           <img src={Icon4} alt="Icon4" />
         </div>
-
+          {/*Eye icon */}
         <div className="icons icon1 row-start-5 col-span-2 md:col-span-3 xl:pb-2 2xl:w-[100px] ">
           <img src={Icon1} alt="Icon1" />
         </div>
-
+          {/*CLock icon */}
         <div className="icons icon2 row-start-5 col-span-3 md:col-span-3 xl:pb-2 2xl:w-[100px]">
           <img src={Icon2} alt="Icon2" />
         </div>
-
+           {/* communication icon*/}
         <div className="icons icon3 row-start-5 col-span-4 md:col-span-3 xl:pb-2 2xl:w-[100px]">
           <img src={Icon3} alt="Icon3" />
         </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
           {showAnimation && (
             <h2
             
-              className={`leading-[55px] mt-3 transition-all ready-text text-left font-bold text-[55px] mb-5 md:mt-0 md:text-[60px] xl:text-[90px] 2xl:text-[120px] 2xl:mt-2 2xl:pt-3 duration-1000 md:text-8xl  relative
+              className={`leading-[55px] mt-3 transition-all ready-text text-left font-bold text-[55px] mb-5 md:mt-0 md:text-[60px] xl:text-[90px] 2xl:text-[100px] 2xl:mt-2 2xl:pt-3 duration-1000 md:text-8xl  relative
                 ${showAnimation ? 'bg-new-adventures xl:pt-[1.5rem]  final-position-adventures slide-in-right scale-110' : 'initial-position'}`}>
               New Challenges
             </h2>
@@ -90,96 +90,3 @@ const Contact = () => {
 
 export default Contact;
 
-
-// const Contact = () => {
-//   const [showAnimation, setShowAnimation] = useState(false);
-//   const contactRef = useRef(null);
-
-//   useEffect(() => {
-//     const observer = new IntersectionObserver(
-//       (entries) => {
-//         const entry = entries[0];
-//         if (entry.isIntersecting) {
-//           setShowAnimation(true);
-//         }
-//       },
-//       { threshold: 0.2 } // Start animation when 20% of the component is visible
-//     );
-
-//     if (contactRef.current) {
-//       observer.observe(contactRef.current);
-//     }
-
-//     // Cleanup observer
-//     return () => {
-//       if (contactRef.current) {
-//         observer.unobserve(contactRef.current);
-//       }
-//     };
-//   }, []);
-
-//   return (
-//     <div ref={contactRef} className="h-screen xl:h-screen 2xl:h-[90%] container contact-section">
-//       {/* Grid Layout with Empty Columns for Large Screens */}
-//       <div className=" flex flex-row lg:grid lg:grid-cols-2 2xl:grid-cols-12 w-full h-full">
-        
-//         {/* Empty Left Column for Larger Screens */}
-//         <div className="hidden 2xl:block xl:col-span-1 2xl:col-span-2"></div>
-        
-//         {/* Animation Section (Centered) */}
-//         <div className="text-animation-container flex-row lg:col-span-1 2xl:col-span-5 flex items-start relative 2xl:mt-10">
-//           {showAnimation && (
-//             <h2 className={`transition-all duration-1000 md:text-[40px] text-6xl md:text-8xl xl:text-[60px] font-bold text-white
-//               ${showAnimation ? 'final-position-ready slide-in-left' : 'initial-position'}`}>
-//               Ready for
-//             </h2>
-//           )}
-//           {showAnimation && (
-//             <h2
-//               style={{
-//                 backgroundColor:"black",
-//                 backgroundSize: 'cover',
-//                 backgroundPosition: 'center',
-//                 color: 'transparent',
-//                 WebkitBackgroundClip: 'text',
-//                 backgroundClip: 'text',
-//               }}
-//               className={`transition-all font-bold md:text-[45px] text-[50px] xl:text-[80px] 2xl:mt-12 2xl:pt-3 duration-1000 md:text-8xl text-white relative lg:absolute
-//                 ${showAnimation ? 'bg-new-adventures final-position-adventures slide-in-right scale-110' : 'initial-position'}`}>
-//               New Challenges
-//             </h2>
-//           )}
-//         </div>
-
-//         {/* Contact Info Section (Centered) */}
-//         <div className="contact-info-section col-span-1 2xl:col-span-5 pl-8 md:pl-0 text-white flex items-center">
-//           <div className="contact-icon-column w-full">
-//             <div className="triangle-left"></div>
-//             <h1 className="contact-title">
-//               LET'S <br />
-//               CONNECT
-//             </h1>
-//             <div className="contact-details pl-28 flex flex-col justify-center">
-//               <div className="contact-info">
-//                 <p className="text-3xl">
-//                   <FontAwesomeIcon icon={faPhone} className="contact-icon" /> +123-456-7890
-//                 </p>
-//                 <p className="text-3xl">
-//                   <FontAwesomeIcon icon={faEnvelope} /> hello@reallygreatsite.com
-//                 </p>
-//                 <p className="text-3xl">
-//                   <FontAwesomeIcon icon={faMapMarkerAlt} /> Berlin
-//                 </p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         {/* Empty Right Column for Larger Screens */}
-//         <div className="hidden 2xl:block lg:col-span-2 xl:col-span-1 2xl:col-span-2"></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Contact;
