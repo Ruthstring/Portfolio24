5529151c38f3598a492145564e1aaffeee71ae52
Ruth Cuellar <ruthcllp@gmail.com>
2025-04-07 12:44:43 +0200

    modifications in project 1 layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,9 +5,10 @@ import Project2 from "./Project2";
 const Projects =()=>{
     return(
         <>
-        
-        <div className="container pt-20 xl:pt-3">
-           
+         {/* Title */}
+         <h1 className="text-start projects-title col-span-12 xl:col-span-12 xl:col-start-1  text-4xl md:text-5xl mt-10 md:pb-0 ml-4 lg:ml-[0.90em] ">Projects</h1>
+        <div className="container pt-6 lg:pt-20 xl:pt-3">
+          
         <Project1 />
         </div>
         <div className="container pt-20" >
